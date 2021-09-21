c217ee8ef4502730c118c37ce9e3f5c7ee5856e7
chu-young <ttheko101@gmail.com>
2021-09-21 10:14:31 +0200

    added the functioning contact form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Projects from './Pages/Projects';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Experience from './Pages/Experience';
+import ContactPage from './Pages/ContactPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 <About/>
 <Projects/>
 <Experience/>
+<ContactPage/>
 
 </main>
 
